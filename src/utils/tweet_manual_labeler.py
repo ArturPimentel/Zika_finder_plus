3c72235797fc745b6042ee762ec984a9cadb6389
ArturPimentel <arturpimentel81@gmail.com>
2016-07-18 16:55:39 -0400

    Manual Labeler now doesn't require to press Enter in order to label text

--- src/utils/tweet_manual_labeler.py
+++ src/utils/tweet_manual_labeler.py
@@ -1,18 +1,22 @@
 import json
+import msvcrt
 
 if __name__ == '__main__':
 	tweets_texts = []
 	pos = 1
-	with open("../../data/dataset_samples/zika_2000tweets.json") as tweets_src_f:
-		with open("../../data/samples_texts/zika_2000sample_ml.txt", "w") as tweets_sample_f:
+	with open("../../data/dataset_samples/zika_3000tweets.json") as tweets_src_f:
+		with open("../../data/samples_texts/zika_3000sample_ml.txt", "w") as tweets_sample_f:
 			for line in tweets_src_f:
 				jtweet = json.loads(line)
 				text = jtweet["text"].encode('utf-8') + "\n"
-				resp = raw_input(str(pos) + text)
+				print str(pos) + " " + text
+				input_char = msvcrt.getch()
+				#resp = raw_input(str(pos) + " " + text)
+				print input_char
 
 				pos += 1
-
-				tweets_sample_f.write(resp + " ")
+				tweets_sample_f.write(input_char + " ")
+				#tweets_sample_f.write(resp + " ")
 				tweets_sample_f.write(text)
 				tweets_sample_f.write('\n==============================================================================================================================================\n')
 

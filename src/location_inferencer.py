224fd60b3814eda209b9d7a0412fd4e21fcf1361
ArturPimentel <arturpimentel81@gmail.com>
2016-07-19 16:58:21 -0400

    Corrected final bugs and added some comments

--- src/location_inferencer.py
+++ src/location_inferencer.py
@@ -8,6 +8,12 @@ from datetime import datetime
 from pprint import pprint
 from pprint import pformat
 
+def get_googlemaps_keys(file_path):
+	with open(file_path) as keys_file:
+		keys = keys_file.readlines()
+		keys = [x.strip('\n') for x in keys]
+		return keys
+
 def get_twitter_keys(file_path):
 	with open(file_path) as twitter_keys_f:
 		twitter_keys_s = twitter_keys_f.read()
@@ -18,99 +24,118 @@ def most_common(lst):
 
 if __name__ == '__main__':
 	# Prepare APIs
-	gmaps = googlemaps.Client(key='AIzaSyCndwIGj8tmXEdXGyvMhXvWWhh0iTiqius')
-	keys = get_twitter_keys("../apikeys/twitter_keys.json")
-	api = TwitterAPI(keys['consumer_key'],
-					 keys['consumer_secret'],
-					 keys['access_token'],
-					 keys['access_token_secret'])
+	gmaps_keys = get_googlemaps_keys('../apikeys/googlemaps_keys.txt')
+	gmaps_keys_idx = 0
+	gmaps = googlemaps.Client(key=gmaps_keys[gmaps_keys_idx])
+	
+	twitter_keys = get_twitter_keys("../apikeys/twitter_keys.json")
+	twitter = TwitterAPI(twitter_keys['consumer_key'],
+					 twitter_keys['consumer_secret'],
+					 twitter_keys['access_token'],
+					 twitter_keys['access_token_secret'])
 
-	# Set control variables
-	n_locs_inf = 0
-	TWITTER_API_LIMIT = 12
-	SLEEP_TIME = 900
+	# Set control variable
+	SLEEP_TIME = 900 # (seconds) Twitter rates renew every 15 minutes
 
-	with open('../data/dataset_samples/geotag_tweets3.json') as f, \
+	with open('../data/dataset_samples/geotag_tweets3.json') as dataset_f, \
 		 open('../data/loc_results/gt_cities2.txt', 'w') as gt_locs_f, \
 		 open('../data/logs/loc_infer_log.txt', 'a') as log:
 
-		for i, line in enumerate(f):
-			if i >= n_locs_inf and i < n_locs_inf + TWITTER_API_LIMIT:
-				tweet = json.loads(line)
+		for i, line in enumerate(dataset_f):
+			tweet = json.loads(line)
 
+			# First, translate coordinates from tweet to the city where it belongs
+			got_reverse_geocode = False
+			while not got_reverse_geocode:
 				try:
 					reverse_geocode_result = gmaps.reverse_geocode(tuple(tweet['coordinates']['coordinates'])[::-1])
+					got_reverse_geocode = True
 				except:
-					gmaps = googlemaps.Client(key='AIzaSyBUU2cnAhQ0vNPqxNpNK_PNwKRW5kxwwbk')
-					reverse_geocode_result = gmaps.reverse_geocode(tuple(tweet['coordinates']['coordinates'])[::-1])
+					# If that key expired, try this one. Limit is 2400 requests per day per key for gmaps
+					gmaps_keys_idx += 1
+					gmaps = googlemaps.Client(key=gmaps_keys[gmaps_keys_idx])
 
-				if reverse_geocode_result != None:
-					has_city = False
-					for place in reverse_geocode_result:
-						place_type = place['address_components'][0]['types']
-						if cmp(place_type, [u'locality', u'political']) == 0:
-							gt_locs_f.write(tweet['user']['screen_name'].encode('utf-8') + "," + place['address_components'][0]['short_name'].encode('utf-8'))
-							has_city = True
-					if has_city == False:
-						gt_locs_f.write(tweet['user']['screen_name'].encode('utf-8') + ",NO_CITY")
-				else:
-					gt_locs_f.write(tweet['user']['screen_name'].encode('utf-8') + ",NO_RESULT")
-
-				got_friends = False
-				friends_ids = ""
-				while not got_friends:
-					try:
-						friends_r = api.request('friends/ids', {'count':100, 'screen_name': tweet['user']['screen_name']})
-						got_friends = True
-					except TwitterError.TwitterRequestError:
-						print "Trying next friends request in 15 minutes\n"
-						log.write("Trying next friends request in 15 minutes\n")
-						time.sleep(SLEEP_TIME)
-
-				got_followers = False
-				while not got_followers:
-					try:
-						followers_r = api.request('followers/ids', {'count':1000, 'screen_name':tweet['user']['screen_name']})
-						got_followers = True
-					except TwitterError.TwitterRequestError:
-						print "Trying next followers request in 15 minutes\n"
-						log.write("Trying next followers request in 15 minutes\n")
-						time.sleep(SLEEP_TIME)
-
-				print friends_r, followers_r
-				try:
-					friends = set(friends_r.response.json()["ids"])
-				except KeyError:
-					print "Trying next friends iterator request in 15 minutes\n"
-					log.write("Trying next friends iterator request in 15 minutes\n")
-					time.sleep(SLEEP_TIME)
-					continue
-
-				try:
-					followers = set(followers_r.response.json()["ids"])
-				except KeyError:
-					print "Trying next followers iterator request in 15 minutes\n"
-					log.write("Trying next followers iterator request in 15 minutes\n")
-					time.sleep(SLEEP_TIME)
-					continue
-
-
-				connections = friends.intersection(followers)
-				connections_ids = ""
-				for connection in connections:
-					connections_ids += "," + str(connection)
-
-				connections_locations = []
-				r = api.request('users/lookup', {'user_id': connections_ids[1:]})
-				for item in r.get_iterator():
-					if 'location' in item:
-						if item['location'] != "":
-							connections_locations.append(item['location'].split(",")[0])
-
-				gt_locs_f.write("," + most_common(connections_locations).encode('utf-8') + "\n")
-				n_locs_inf += 1
+			# Retrieve city name from google maps response
+			username = tweet['user']['screen_name'].encode('utf-8')
+			if reverse_geocode_result != None:
+				has_city = False
+				# Find the place in the list that has the city information
+				for place in reverse_geocode_result:
+					place_type = place['address_components'][0]['types']
+					if cmp(place_type, [u'locality', u'political']) == 0:
+						geocode_r_str = place['address_components'][0]['short_name'].encode('utf-8')
+						has_city = True
+				if has_city == False:
+					geocode_r_str = "NO_CITY"
 			else:
-				print "Trying next general request in 15 minutes\n"
-				log.write("Trying next general request in 15 minutes\n")
-				n_locs_inf += TWITTER_API_LIMIT
+				geocode_r_str = "NO_RESULT"
+
+			# Request friends ids
+			got_friends = False
+			friends_ids = ""
+			while not got_friends:
+				try:
+					friends_r = twitter.request('friends/ids',
+						{'count':100, 'screen_name': tweet['user']['screen_name']})
+					got_friends = True
+				except TwitterError.TwitterRequestError:
+					print "Trying next friends request in 15 minutes\n"
+					log.write("Trying next friends request in 15 minutes\n")
+					time.sleep(SLEEP_TIME)
+			# Prepare a list of friends ids from the response
+			try:
+				friends = set(friends_r.response.json()["ids"])
+			except KeyError:
+				print "Trying next friends iterator request in 15 minutes\n"
+				log.write("Trying next friends iterator request in 15 minutes\n")
 				time.sleep(SLEEP_TIME)
+				continue
+
+			# Request followers ids
+			got_followers = False
+			while not got_followers:
+				try:
+					followers_r = twitter.request('followers/ids',
+						{'count':1000, 'screen_name':tweet['user']['screen_name']})
+					got_followers = True
+				except TwitterError.TwitterRequestError:
+					print "Trying next followers request in 15 minutes\n"
+					log.write("Trying next followers request in 15 minutes\n")
+					time.sleep(SLEEP_TIME)
+			# Prepare a list of followers ids from the response
+			try:
+				followers = set(followers_r.response.json()["ids"])
+			except KeyError:
+				print "Trying next followers iterator request in 15 minutes\n"
+				log.write("Trying next followers iterator request in 15 minutes\n")
+				time.sleep(SLEEP_TIME)
+				continue
+
+			# Get a list with bi-directed connections
+			connections = friends.intersection(followers)
+			connections_ids = ""
+			for connection in connections:
+				connections_ids += "," + str(connection)
+
+			# Get the locations of those connections
+			connections_locations = []
+			got_users = False
+			while not got_users:
+				try:
+					users_r = twitter.request('users/lookup', {'user_id': connections_ids[1:]})
+					got_users = True
+				except TwitterError.TwitterRequestError:
+					print "Trying next users request in 15 minutes\n"
+					log.write("Trying next users request in 15 minutes\n")
+					time.sleep(SLEEP_TIME)
+
+			for item in users_r.response.json():
+				if 'location' in item:
+					if item['location'] != "":
+						# First substring before a comma usually is a city
+						connections_locations.append(item['location'].split(",")[0])
+
+			inference_r_str = most_common(connections_locations).encode('utf-8') + "\n"
+
+			gt_locs_f.write(username + "," + geocode_r_str + ","  + inference_r_str + "\n")
+			print username + "," + geocode_r_str + ","  + inference_r_str
